88da22741a1671952aa9cd9a1c7d007fca6ad28a
malcolm-a <aridoryjr@icloud.com>
2024-06-24 12:15:04 +0200

    birth and kill cells

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,12 @@ fn randomize_grid(grid: &mut Vec<Vec<i8>>) {
     }
 }
 
+/// Replaces the cell's value with 1
+fn birth(grid: &mut Vec<Vec<i8>>, x: usize, y: usize) {
+    grid[x][y] = 1;
+}
+
+/// Replaces the cell's value with 0
+fn kill(grid: &mut Vec<Vec<i8>>, x: usize, y: usize) {
+    grid[x][y] = 0;
+}
